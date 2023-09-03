8208bb1d816eb3cf833e36cc3892e9f281fb4575
gustavo brandassi <gustavobrandassi@icloud.com>
2023-09-03 20:45:31 -0300

    segundo commit

--- 14).py
+++ 14).py
@@ -1,4 +1,3 @@
-
 salario_inicial = float(input("Digite o salário inicial do funcionário: "))
 ano = 1996
 salario = salario_inicial
@@ -6,7 +5,7 @@ aumento_percentual = 1.5
 
 while ano <= 2023:
     salario += salario * (aumento_percentual / 100)
-    aumento_percentual *= 2
+    aumento_percentual += 1.5
     ano += 1
 
 print(f"O salário atual do funcionário em 2023 é R${salario:.2f}")
